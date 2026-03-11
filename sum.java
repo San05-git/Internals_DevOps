38fea040139896f36a88063bea489e83e1815436
Samruddhi-1605 <samruddhisridhar2005@gmail.com>
2026-03-11 22:01:13 +0530

    Fix syntax errors and update method call in sum.java

--- sum.java
+++ sum.java
@@ -1,9 +1,9 @@
-class sum{
+class sum {
   public class add(int x,int y){
   int z=x+y;
   System.out.println("The value is:"+z);
 }
 public static void main(String[] args){
-  x=10,y=5;
+   add(10,5);
 }
 }
